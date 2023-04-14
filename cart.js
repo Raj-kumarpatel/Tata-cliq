276515e3b0fe4956ccad5d6101c5e8f60702bae5
mdghufran1729 <mdghufran1729@gmail.com>
2023-04-14 22:40:19 +0530

    cart

--- cart.js
+++ cart.js
@@ -1,30 +1,3 @@
-// '''''''''''''''''''''''''''''''''''''''''''''trial'''''''''''''''''''''''''''''''''''''''''''''''
-// res()
-function res(f){
-  
-    var arr=[];
-   
-    obj={
-        image:"https://www.shutterstock.com/image-photo/summer-beach-holiday-online-shopping-260nw-461355724.jpg",
-        description:"this is the best t-shirt you ever got",
-        price:900,
-        size:7,
-        color:"mixup",
-        discounted_price:800
-    }
-   
-    arr.push(obj);
-    obj={
-        image:"https://www.shutterstock.com/image-photo/summer-beach-holiday-online-shopping-260nw-461355724.jpg",
-        description:"this is the best t-shirt you ever got",
-        price:900,
-        size:7,
-        color:"mixup",
-        discounted_price:800
-    }
-    arr.push(obj);
-    localStorage.setItem("bagItem",JSON.stringify(arr))
-}
 
 
 // -------------------------------trial------------------------------------------------------------------
@@ -35,9 +8,9 @@ window.addEventListener('scroll',function(){
     navbar2.classList.toggle('sticky1',window.scrollY)
     })
 
-document.querySelector('.accounts').addEventListener('mouseover',function(){
-    document.querySelector('.speech-bubble').classList.add('display')
-})
+// document.querySelector('.accounts').addEventListener('mouseover',function(){
+//     document.querySelector('.speech-bubble').classList.add('display')
+// })
 // document.querySelector('.keep').addEventListener('mouseover',function(){
 //     document.querySelector('.speech-bubble').classList.add('display')
 // })
@@ -46,7 +19,7 @@ document.querySelector('.accounts').addEventListener('mouseover',function(){
 // })
 
 
-var bagItem = localStorage.getItem('bagItem');
+var bagItem = localStorage.getItem('bagItem')||[];
 
 var productInfo = document.querySelector('.product-info');
 var productSide = document.querySelector('.product-side');
@@ -77,17 +50,19 @@ else{
           <img src="${bagItem[i].image}" alt="">
         </div>
         <div class="details">
+        <pclass="name">${bagItem[i].name}</p>
           <p class="product-name">${bagItem[i].description}</p>
+         
           <p class="price">₹${bagItem[i].discounted_price}<p class="dprice">₹${bagItem[i].price}</p></p>
           <div class="counter">
             Quantity:
-            <span class="down" onClick='decreaseCount(event, this,${i}) '>-</span>
+            <span class="down" onClick='decreaseCount(event, this,${i})'>-</span>
             <input type="text" value="1">
             <span class="up" onClick='increaseCount(event, this , ${i})'>+</span>
           </div>
           <div class="color-size">
             <div class="rang">Color: <p class="color">${bagItem[i].color}</p></div>
-            <div class="length">Size: <p class="size">${bagItem[i].size}</p></div>
+            
           </div>
           <div class="line"></div>
           
@@ -106,16 +81,16 @@ else{
         </div>
       </div>`
       productSide.append(div)
-     var quant=JSON.parse(localStorage.getItem("quantity"));
-     sum += Number(bagItem[i].price)*quant;
-    //  window.reload();
-    diff += Number(bagItem[i].discounted_price)*quant;
-    
-    
      
+      
+     var quant=JSON.parse(localStorage.getItem("quantity"));
+     sum += Number(bagItem[i].price);
+    //  window.reload();
+    diff += Number(bagItem[i].discounted_price);
+    
     
     }
-     
+    
     productInfo.append(productSide)
     productInfo.getElementsByClassName.flexDirection = 'row-reverse'
     document.querySelectorAll('.total')[0].innerHTML = sum;
@@ -130,8 +105,9 @@ else{
       dprice: (diff)
     }
     localStorage.setItem('priceobj',JSON.stringify(priceobj))
-     localStorage.setItem('bagtotal',JSON.stringify(priceobj.dprice))
-    
+    localStorage.setItem('bagtotal',JSON.stringify(priceobj.dprice))
+    console.log(priceobj);
+
   }
 }
 //counter
@@ -141,45 +117,42 @@ var z=0
 function increaseCount(a, b , idx) {
   
   var input = b.previousElementSibling;
-  var value = parseInt(input.value);
+  var value = parseInt(input.value, 10);
   value = isNaN(value) ? 0 : value;
   value++;
-//   var dprice=Number(bagItem[i].price)*value;
-  
-  localStorage.setItem("quantity",JSON.stringify(value));
   input.value = value;
-//   x +=  Number(JSON.parse(bagItem)[idx].price)
-//   z += Number(JSON.parse(a.localStorage.bagItem)[idx].discounted_price)
+  x +=  Number(JSON.parse(localStorage.bagItem)[idx].price)
+  z += Number(JSON.parse(localStorage.bagItem)[idx].discounted_price)
   y(+x,+z)
 }
 
 function decreaseCount(a, b , idx) {
   var input = b.nextElementSibling;
-  var value = parseInt(input.value);
+  var value = parseInt(input.value, 10);
   if (value > 1) {
     value = isNaN(value) ? 0 : value;
     value--;
     input.value = value;
-    // x -=  Number(JSON.parse(a.path[11].localStorage.bagItem)[idx].price)
-    // z -= Number(JSON.parse(a.path[11].localStorage.bagItem)[idx].discounted_price)
+    x -=  Number(JSON.parse(localStorage.bagItem)[idx].price)
+    z -= Number(JSON.parse(localStorage.bagItem)[idx].discounted_price)
+    console.log(x,z)
     y(+x,+z)
     
   }
 }
 
 function y(x,z){
-  document.querySelectorAll('.total')[0].innerHTML = sum;
-  document.querySelectorAll('.total')[1].innerHTML = sum;
-  document.querySelector('#dif').innerHTML = (sum) - (diff);
-  document.querySelector('#fl').innerHTML = (sum ) - (diff);
-  document.querySelector('.tot').innerHTML =  diff;
+  document.querySelectorAll('.total')[0].innerHTML = sum +x;
+  document.querySelectorAll('.total')[1].innerHTML = sum +x;
+  document.querySelector('#dif').innerHTML = (sum +x) - (diff+z);
+  document.querySelector('#fl').innerHTML = (sum +x) - (diff+z);
+  document.querySelector('.tot').innerHTML =  diff+z;
   var priceobj = {
     tprice: (sum + x),
     dvalue: ((sum +x) - (diff+z)),
     dprice: (diff + z)
   }
   localStorage.setItem('priceobj',JSON.stringify(priceobj))
-  
   console.log(priceobj);
 
 }
@@ -198,6 +171,7 @@ function coupon(c){
       dvalue: newdis,
       dprice: newpay
     }
+    document.querySelector('#fl').innerHTML = priceobj.dvalue;
     localStorage.setItem('priceobj',JSON.stringify(priceobj))
     f=0
   }
@@ -207,13 +181,10 @@ var payable = document.querySelector('.tot');
 console.log(payable.value);
 function rem(idy){
   console.log(bagItem[idy]);
-  bagItem.splice(idy,1)
+  bagItem.splice(idy,idy+1)
   if(bagItem.length == 0){
    document.querySelector('.empty').style.display = 'flex';
   }
   localStorage.setItem('bagItem',JSON.stringify(bagItem))
   location.reload();
 }
-
-// let loggedIn = JSON.parse(localStorage.getItem("loginData"));
-// document.querySelector(".UserName").textContent = loggedIn[0].name;
